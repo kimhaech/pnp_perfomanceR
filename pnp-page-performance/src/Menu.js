0ff0f4e831cd286ded884b9e3c9dc43a5320ce69
kimhaech <epiclog@hufs.ac.kr>
2021-09-22 23:00:06 +0900

    bar

---
+++ pnp-page-performance/src/Menu.js
@@ -0,0 +1,20 @@
+function Menu() {
+  return (
+    <header>
+      <div>PnP logo</div>
+
+      <nav>
+        <ul>
+          <li>홈</li>
+          <li>학회소개</li>
+          <li>구성원</li>
+          <li>학회활동</li>
+          <li>학회실적</li>
+          <li>문의하기</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Menu
