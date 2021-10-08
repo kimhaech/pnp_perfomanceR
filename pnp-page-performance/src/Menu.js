f8056283033680453040487073f979ef5b68d8b1
kimhaech <epiclog@hufs.ac.kr>
2021-10-08 21:20:27 +0900

    add
    for ui

--- pnp-page-performance/src/Menu.js
+++ pnp-page-performance/src/Menu.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 function Menu() {
   // 메뉴 버튼
   return (
@@ -28,3 +30,20 @@ function Menu() {
 }
 
 export default Menu
+// {/* <header className="text-gray-700 body-font border-b border-gray-200">
+//             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+//                 <a
+//                     className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+//                     href="/"
+//                 >
+//                     <Logo className="w-10 h-10" />
+//                     {/* <span className="ml-3 text-xl">PnP</span> */}
+//                 </a>
+//                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+//                     <a className="mr-5 hover:text-gray-900">ABOUT</a>
+//                     <a className="mr-5 hover:text-gray-900">ACTIVITY</a>
+//                     <a className="mr-5 hover:text-gray-900">MEMBERS</a>
+//                     <a className="mr-5 hover:text-gray-900">RECRUIT</a>
+//                 </nav>
+//             </div>
+//         </header> */}
